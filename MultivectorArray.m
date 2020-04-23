47941915d2d7f970fd75760a86adc1c6b45779db
Nikolay Murzin <murzin.nikolay@gmail.com>
2020-04-24 01:05:49 +0300

    Improve MultivectorArray

--- MultivectorArray.m
+++ MultivectorArray.m
@@ -12,27 +12,41 @@ MultivectorArray[vs_ /; ArrayQ[vs, _, MatchQ[_Multivector]], shape: {___Integer}
     MultivectorArray["Components" -> vs, "Shape" -> shape],
     Message[MultivectorArray::badShape, shape, Dimensions[vs]]; $Failed
 ]
+MultivectorArray[vs_ /; ArrayQ[vs, _, MatchQ[_Multivector]]] := MultivectorArray[vs, Dimensions[vs]]
 
-MultivectorArray[v_Multivector, shape: {}] := If[
-    DeleteCases[shape, 0] === {},
-    MultivectorArray["Components" -> v, "Shape" -> shape],
-    Message[MultivectorArray::badShape, shape, {}]; $Failed
-]
+MultivectorArray[v_Multivector, shape_: {}] := MultivectorArray["Components" -> v, "Shape" -> {}]
 
 va_MultivectorArray[opt_String] /; KeyExistsQ[Options[va], opt] := Lookup[Options[va], opt]
 va_MultivectorArray["Rank"] := Length @ va["Shape"]
 
-GeometricProduct[va_MultivectorArray, vb_MultivectorArray] := With[{
-    outer = Outer[GeometricProduct, va["Components"], vb["Components"]],
+mapComponents[f_, va_MultivectorArray] := MultivectorArray[
+    If[va["Rank"] == 0, f[va["Components"]], Map[f, va["Components"], {va["Rank"]}]],
+    va["Shape"]
+]
+
+expandDims[va_MultivectorArray, sign_Integer: 1, dim_Integer: -1] /; sign != 0 := With[{
+    shape = Insert[va["Shape"], Sign[sign], dim]
+},
+    MultivectorArray[If[va["Rank"] == 0, {va["Components"]}, ArrayReshape[va["Components"], Abs[shape]]], shape]
+]
+
+MultivectorArray /: f_[va_MultivectorArray, vb_MultivectorArray] /; va["Rank"] > 0 && vb["Rank"] > 0 := With[{
+    outer = Outer[f, va["Components"], vb["Components"]],
     shape = Join[va["Shape"], vb["Shape"]],
     shapeContraction = {va["Rank"], va["Rank"] + 1}
 },
     If[ MatchQ[{Last @ va["Shape"], First @ vb["Shape"]}, {x_ ? Negative, y_ ? Positive} /; x == -y],
-        MultivectorArray[Total[outer, shapeContraction], Delete[shape, List /@ shapeContraction]],
+        MultivectorArray[TensorContract[outer, shapeContraction], Delete[shape, List /@ shapeContraction]],
         MultivectorArray[outer, shape]
     ]
 ]
+MultivectorArray /: f_[va_MultivectorArray, vb_MultivectorArray] := f[expandDims[va, -1], expandDims[vb, 1, 1]]
+MultivectorArray /: f[vas__MultivectorArray] := Fold[f, {vas}]
 
+
+unaryOps = Grade | Reverse | Involute | Conjugate | LeftDual | RightDual | Dual | MultivectorTransform;
+MultivectorArray /: (f: unaryOps)[va_MultivectorArray, args___] :=
+    mapComponents[f[#, args] &, va]
 
 PackageExport["ShapeContract"]
 
@@ -42,27 +56,11 @@ ShapeContract[va_MultivectorArray] := With[{
     MultivectorArray[Fold[Total, va, shapeContractions], Delete[va["Shape"], Thread @ shapeContractions]]
 ]
 
-(*
-MultivectorArray /: MakeBoxes[va: MultivectorArray[OptionsPattern[]], StandardForm] := With[{
-    box =
-        If[ va["Shape"] === {}, 
-            ToBoxes[va["Components"], StandardForm],
-            If[ First[va["Shape"]] > 0,
-                RowBox[{"[", GridBox[{ToBoxes[MultivectorArray[#, Rest[va["Shape"]]], StandardForm]} & /@ va["Components"]], "]"}],
-                RowBox[{"(", GridBox[{ToBoxes[MultivectorArray[#, Rest[va["Shape"]]], StandardForm] & /@ va["Components"]}], ")"}]
-            ]
-        ],
-    tooltip = RowBox[{"MultivectorArray ", ToBoxes @ va["Shape"]}]
-},
-    InterpretationBox[box, va, Tooltip -> tooltip]
-]
-*)
-
 shapeGridBoxes[array_, shape_] := If[shape === {},
     Slot[array],
     If[First[shape] > 0,
-        RowBox[{"[", GridBox[{shapeGridBoxes[#, Rest[shape]]} & /@ array], "]"}],
-        RowBox[{"(", GridBox[{shapeGridBoxes[#, Rest[shape]] & /@ array}], ")"}]
+        RowBox[{"(", GridBox[{shapeGridBoxes[#, Rest[shape]]} & /@ array],   ")"}],
+        RowBox[{"[", GridBox[{shapeGridBoxes[#, Rest[shape]] & /@ array}], "]"}]
     ]
 ]
 
