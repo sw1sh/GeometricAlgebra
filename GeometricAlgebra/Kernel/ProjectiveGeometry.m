3399e92f4850a3eaca2e609b692125583667504d
Nikolay Murzin <nikm@wolfram.com>
2025-05-01 03:32:19 +0300

    Add more PGA definitions

--- GeometricAlgebra/Kernel/ProjectiveGeometry.m
+++ GeometricAlgebra/Kernel/ProjectiveGeometry.m
@@ -5,6 +5,33 @@ PackageImport["Wolfram`GeometricAlgebra`"]
 PackageExport[$PGA]
 PackageExport[RegionPGA]
 PackageExport[PGARegions]
+PackageExport[PGAVectorQ]
+PackageExport[PGAPoint]
+PackageExport[PGALine]
+PackageExport[PGAPlane]
+
+PackageExport[PGABulk]
+PackageExport[PGARightBulkDual]
+PackageExport[PGALeftBulkDual]
+PackageExport[PGAWeight]
+PackageExport[PGARightWeightDual]
+PackageExport[PGALeftWeightDual]
+
+PackageExport[PGABulkNorm]
+PackageExport[PGAWeightNorm]
+PackageExport[PGANorm]
+
+PackageExport[RightInteriorProduct]
+PackageExport[LeftInteriorProduct]
+PackageExport[RightInteriorAntiProduct]
+PackageExport[LeftInteriorAntiProduct]
+PackageExport[PGAProjection]
+PackageExport[PGARejection]
+PackageExport[PGABulkContraction]
+PackageExport[PGAWeightContraction]
+PackageExport[PGABulkExpansion]
+PackageExport[PGAWeightExpansion]
+
 
 
 $PGA = GeometricAlgebra[3, 0, 1, "Format" -> "PGA"]
@@ -16,6 +43,9 @@ PGAVectorQ[v_Multivector] := v["Signature"] === {3, 0, 1}
 
 pseudoVector[v : {_, _, _}] := Reverse[v] {1, -1, 1}
 
+
+(* Constructors *)
+
 PGAPoint[p : {_, _, _}, w_ : 1] := Grade[Append[p, w], 1, $PGA]
 
 PGALine[v : {_, _, _}, m : {_, _, _}] := eps ** Grade[v, 1, $PGA] + Grade[- m, 1, $PGA]["Dual"]
@@ -23,15 +53,29 @@ PGALine[v : {_, _, _}, m : {_, _, _}] := eps ** Grade[v, 1, $PGA] + Grade[- m, 1
 PGAPlane[n : {_, _, _}, d_] := Grade[Prepend[pseudoVector[n], - d], 3, $PGA]
 
 
+(* Unary operations *)
+
 PGABulk[v_Multivector] := Multivector[KeySelect[v["Association"], FreeQ[4]], v["GeometricAlgebra"]]
 
+PGARightBulkDual[v_Multivector] := PGABulk[v]["RightComplement"]
+
+PGALeftBulkDual[v_Multivector] := PGABulk[v]["LeftComplement"]
+
 PGAWeight[v_Multivector] := Multivector[KeySelect[v["Association"], Not @* FreeQ[4]], v["GeometricAlgebra"]]
+
+PGARightWeightDual[v_Multivector] := PGAWeight[v]["RightComplement"]
+
+PGALeftWeightDual[v_Multivector] := PGAWeight[v]["LeftComplement"]
+
+(* Norms *)
 
 PGABulkNorm[a_Multivector] := Sqrt[a ** a["Reverse"]]
 
 PGAWeightNorm[a_Multivector] := Sqrt[AntiGeometricProduct[a, a["AntiReverse"]]]
  
 PGANorm[a_Multivector] := PGABulkNorm[a] + PGAWeightNorm[a]
+
+(* Binary operations *)
 
 RightInteriorProduct[a_Multivector, b_Multivector] := Vee[a, b["RightComplement"]]
 
@@ -45,6 +89,16 @@ PGAProjection[a_Multivector, b_Multivector] := LeftInteriorProduct[RightInterior
 
 PGARejection[a_Multivector, b_Multivector] := LeftInteriorAntiProduct[RightInteriorAntiProduct[PGAWeight[b], a], b]
 
+PGABulkContraction[a_Multivector, b_Multivector] := Vee[a, PGARightBulkDual[b]]
+
+PGAWeightContraction[a_Multivector, b_Multivector] := Vee[a, PGARightWeightDual[b]]
+
+PGABulkExpansion[a_Multivector, b_Multivector] := Wedge[a, PGARightBulkDual[b]]
+
+PGAWeightExpansion[a_Multivector, b_Multivector] := Wedge[a, PGARightWeightDual[b]]
+
+
+(* Region constructions *)
 
 Attributes[RegionPGA] = {Listable}
 
@@ -60,6 +114,8 @@ RegionPGA[InfinitePlane[p_, {u_, v_}]] := Wedge[RegionPGA[Point[p]], RegionPGA[P
 
 RegionPGA[Hyperplane[n_, d_]] := PGAPlane[n, d]
 
+
+(* Region export *)
 
 PGARegions[v_Multivector] /; PGAVectorQ[v] := Quiet @ With[{
     c1 = v["Coordinates", 1],
